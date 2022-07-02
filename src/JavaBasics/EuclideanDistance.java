6b409431ee6aa71809df66ae404e32be350b507c
MohanRaj <mraj86698@gmail.com>
2022-07-02 20:37:44 +0530

    Added Day 2 Problems

---
+++ src/JavaBasics/EuclideanDistance.java
@@ -0,0 +1,16 @@
+package JavaBasics;
+
+public class EuclideanDistance {
+
+	public static void main(String[] args) {
+		// input point coordinates
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+
+		// double distance = Math.sqrt((x * x) + (y * y));
+		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		System.out.println("Distance from (0, 0) to (" + x + ", " + y + ") is " + distance);
+
+	}
+
+}
